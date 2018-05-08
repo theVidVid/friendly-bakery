e6d6d8d3f40888b5b9cec526777e123adea6695e
Ian Vidaurre <ianjvidaurre@gmail.com>
2018-05-07 20:18:42 -0400

    Fixed Email form. Successfully sent Bakery catalog.

--- app.rb
+++ app.rb
@@ -40,13 +40,41 @@ end
 
 post "/contact" do
 
-    @name = params[:name]
     @email = params[:email]
   
-    from = Name.new(name: @name)
+    from = Email.new(email: @Email)
     to = Email.new(email: 'ianjvidaurre@gmail.com')
     subject = 'Thank you for signing up for our email list.'
-    content = Content.new(type: 'text/plain', value: @catalog)
+    content = Content.new(type: 'text/plain', value: 'Bakery Email sign-up confirmation.
+        Thank you for your interest in Brooklyn Baked Goods Collective!
+        
+        We aim to provide the best selection of baked goods to sweeten your
+        event, whether it be a birthday, graduation, anniversary, wedding, or even 
+        on your daily commute.
+
+        Please take a peek at our catalog and please allow 24 hours for our team
+        review your order. 
+
+        Cakes
+
+        White round cake topped with sliced fruit - $48
+        Chocolate ganache & strawberry cake - $36
+        Classic chocolate mousse cake - $34
+
+        Cookies
+
+        Salted chocolate chip - $4/$20 single/dozen
+        Salted peanut butter - $4/$20 single/dozen
+        Macadamia nut - $4/$20 single/dozen
+
+        Muffins 
+
+        Brulee Egg Tart - $7
+        Spinach Banana - $5
+        Matcha Green Tea - $5
+        
+        Stay tuned for seasonal menu changes!')
+
     mail = Mail.new(from, subject, to, content)
   
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
@@ -54,11 +82,29 @@ post "/contact" do
     if response.status_code == 401
       error_hash = JSON.parse(response.body)
       @errors = error_hash["errors"]
+      erb :catalog
     end 
 end 
 
+post '/email' do
+    template = (erb :htmlemail, :layout => false)
+    from = Email.new(email: 'info@friendlybakery.com')
+    to = Email.new(email: params[:email])
+    subject = 'Friendly Bakery Catalog'
+    content = Content.new(type: 'text/html', value: template)
+    mail = Mail.new(from, subject, to, content)
+  
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    puts response.status_code
+    puts response.body
+    # puts response.parsed_body
+    puts response.headers
+    erb :email
+  end
+
 #cakes
-london_fog_cake = Cakes.new("london_fog_cake.jpg", "Chocolate cake with Earl Grey buttercream and salted caramel", "$48")
+white_round_cake = Cakes.new("white-round-cake.jpeg", "White round cake topped with sliced fruit", "$48")
 strawberry_chocolate_cake = Cakes.new("Strawberry_Chocolate_Cake.jpg", "Chocolate ganache & strawberry cake", "$36")
 classic_chocolate_mousse = Cakes.new("chocolate_cake.jpg", "Classic chocolate mousse cake", "$34")
 
